2216bef5f5205f9b5e73609313292e1bf56a99ff
GusCornejo <gustavoxcornejo@gmail.com>
2026-04-08 10:39:00 -0400

    Updated the resend config

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,19 +1,22 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-
-    const  {name, email, subject, body } = await request.json();
+  try {
+    const { name, email, subject, body } = await request.json();
     await resend.emails.send({
-        from: "onboarding@resend.dev",
-        to: "gustavoxcornejo@gmail.com",
-        subject: subject,
-        html: 
-        `<div>
-            <h1>${name}</h1>
-            <h2>${email}</h2>
-            <h3>${body}</h3>
-        </div>`,
+      from: "Gus Cornejo <noreply@mail.guscornejo.com>",
+      to: "gus.cornejo@icloud.com",
+      subject: subject,
+      html: `<div>
+        <h1>${name}</h1>
+        <h2>${email}</h2>
+        <h3>${body}</h3>
+      </div>`,
     });
+    return Response.json({ success: true });
+  } catch (error) {
+    return Response.json({ error: "Failed to send email" }, { status: 500 });
+  }
 }
